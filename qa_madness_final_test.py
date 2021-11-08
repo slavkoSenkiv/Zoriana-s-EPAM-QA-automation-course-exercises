42217c797edfec925363863d60706f060792eed2
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-11-08 22:06:32 +0200

    Update qa_madness_final_test.py

--- qa_madness_final_test.py
+++ qa_madness_final_test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
+# <editor-fold desc="default part, u can skip it">
 search_xpath = '/html/body/div[2]/div/header/div/div[1]/div[2]/div/form/input'
 search_button_xpath = '/html/body/div[2]/div/header/div/div[1]/div[2]/div/form/button'
 product_xpath = '/html/body/div[2]/div/main/section[2]/div/div/div/ul/li'
@@ -7,6 +8,7 @@ path_to_webdriver = '/Users/ysenkiv/Code/access files/chromedriver'
 webdriver = webdriver.Chrome(executable_path=path_to_webdriver)
 webdriver.maximize_window()
 webdriver.get('https://www.board-game.co.uk/')
+# </editor-fold>
 
 
 def try_search(multiplier):
