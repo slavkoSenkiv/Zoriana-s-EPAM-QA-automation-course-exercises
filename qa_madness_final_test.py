b68287d56dc14f26b402d54a6e40b3a64247a6d6
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-11-08 22:44:20 +0200

    Update qa_madness_final_test.py

--- qa_madness_final_test.py
+++ qa_madness_final_test.py
@@ -16,12 +16,12 @@ def try_search(multiplier):
     webdriver.find_element_by_xpath(search_button_xpath).click()
     webdriver.find_element_by_xpath(search_xpath).clear()
     if 'query' in webdriver.current_url:
-        print(f'{"a" * multiplier} works')  # here should be return True
+        print(f'>{"a" * multiplier}< works')  # here should be return True
     else:
-        print(f'{"a" * multiplier} does not work') # here should be return False
+        print(f'>{"a" * multiplier}< does not work')  # here should be return False
 
 
-for x in range(1, 4):
+for x in range(4):
     try_search(x)
 
 
